c9c9044a755f51c0ed98cada86c3b93ef28dbed7
深搜暴力斌 <3197235088@qq.com>
2018-01-19 21:05:34 +0800

    find disk update

--- u盘锁定电脑/u盘锁定电脑/main.cpp
+++ u盘锁定电脑/u盘锁定电脑/main.cpp
@@ -2,8 +2,10 @@
 #include <cstring>
 #include <iostream>
 #include <windows.h>
+#include <wchar.h>
 
 bool check_disk();
+bool check_file(WCHAR* Driver_Path);
 
 int main()
 {
@@ -12,31 +14,25 @@ int main()
 	return 0;
 }
 
+bool check_file(WCHAR* Driver_Path)
+{}
+
 bool check_disk()
 {
-	char diskPath[5] = { 0 };
 	DWORD allDisk = GetLogicalDrives();  //返回一个32位整数，将他转换成二进制后，表示磁盘,最低位为A盘  比如返回00011就表示有a盘和b盘
-
+	WCHAR Driver_Path[4]=TEXT("C:\\");
 	if (allDisk != 0)
 	{
 		for (int i = 1;i<11;i++)     //假定最多有10个磁盘
 		{
 			if ((allDisk & 1) == 1)		//如果有磁盘i
 			{
-				printf("%d\n", i);
-				/*if (GetDriveType(diskPath) == DRIVE_REMOVABLE)
-				{
-					//AfxMessageBox( "检测到U盘", MB_OK );
-					//AfxMessageBox( diskPath, MB_OK );
-
-					if (GetVolumeInformation(diskPath, 0, 0, 0, 0, 0, 0, 0))  //判断驱动是否准备就绪
-					{
-						printf("U盘就绪\n");
-						break;
-					}
-				}*/
+				
+				Driver_Path[0] = 'C' + i - 3;
+				if (GetDriveType(Driver_Path) == DRIVE_REMOVABLE)
+					if (check_file(Driver_Path))
+						return 1;
 			}
-
 			allDisk = allDisk >> 1;
 		}
 	}
